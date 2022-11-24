0af9bd9848ad527232153ec59b2be8c8ddb92696
Rowen Peebles <rowen.peebles@outlook.com>
2022-11-24 14:56:19 +0000

    Adding Python For Loop

---
+++ PY.FlowControl/ForLoop/Example1/Example1.py
@@ -0,0 +1,10 @@
+# Example of Pythin for loop
+
+numbers = [6, 5, 4, 7, 8, 4, 5, 1, 2, 1, 4, 6, 11]
+
+sum = 0
+
+for val in numbers:
+    sum = sum + val
+
+print("The sum is", sum)
---
+++ PY.FlowControl/ForLoop/Example2/Example2.py
@@ -0,0 +1,9 @@
+# For loop with else statement
+
+digits = [0, 1, 5]
+
+for i in digits:
+    print(i)
+
+else:
+    print("No items left")
